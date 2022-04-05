cf7546b5abd48a7b952ff89021e4b5e69d340a64
ShayasTech <shayas.tech@gmail.com>
2022-04-05 23:36:24 +0300

    Project almost completed Added and commended out Black Hovering Effect Added Rainbow Hovering Effect Next Need to Try Black 10% on each Pass

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,54 @@
 console.log("Etch-a-Sketch");
-let canvas = document.querySelector(".canvas");
-let width = canvas.offsetWidth;
-let gridColor = document.querySelector(`input[name="grid"]`);
+let container = document.querySelector(".container");
+let width = container.offsetWidth;
 
-let n = parseInt(prompt("Enter the Size of Grid", 16));
+let reload = document.querySelector("button");
+reload.addEventListener("click", () => {
+    location.reload();
+});
 
+function gridSize() {
+    let n = parseInt(prompt("Enter the Size of Grid (N X N)", 16));
+    if (n > 100) {
+        gridSize();
+    } else {
+        return n;
+    }
+}
+
+let n = gridSize();
 for (let i = 0; i < (n * n); i++) {
     let grid = document.createElement("div");
-    grid.classList.add("grid");
     grid.style.width = (width / n) + "px";
     grid.style.height = (width / n) + "px";
-    // grid.style.border = 1 + "px black solid";
-    grid.style.backgroundColor = "yellow";
-    canvas.appendChild(grid);
+    grid.style.border = "1px solid rgba(0,0,0,0.1)";
+    grid.classList.add("grid");
+    container.appendChild(grid);
 }
+
+let grids = document.querySelectorAll(".grid");
+
+// // Hovering Effect BLACK BACKGROUND
+// grids.forEach(grid => {
+//     grid.addEventListener("mouseover", () => {
+//         grid.style.backgroundColor = "black";
+//     });
+//     grid.addEventListener("mouseleave", () => {
+//         grid.style.backgroundColor = "white";
+//     });
+// });
+
+
+// Hovering Effect RANDOM COLOR BACKGROUND
+function randomRBG() {
+    return Math.floor(Math.random() * 256);
+}
+
+grids.forEach(grid => {
+    grid.addEventListener("mouseover", () => {
+        grid.style.backgroundColor = `rgb(${randomRBG()},${randomRBG()},${randomRBG()})`;
+    });
+    grid.addEventListener("mouseleave", () => {
+        grid.style.backgroundColor = "white";
+    })
+})
