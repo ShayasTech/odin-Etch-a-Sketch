642b5397dc26e700c2043987ba65ce7f832b1fe5
ShayasTech <shayas.tech@gmail.com>
2022-04-06 12:41:38 +0300

    Added 10% Opacity on each mouseover event feature Project Completed

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 console.log("Etch-a-Sketch");
 let container = document.querySelector(".container");
 let width = container.offsetWidth;
+let opacity = 0;
 
 let reload = document.querySelector("button");
 reload.addEventListener("click", () => {
@@ -39,16 +40,25 @@ let grids = document.querySelectorAll(".grid");
 // });
 
 
-// Hovering Effect RANDOM COLOR BACKGROUND
-function randomRBG() {
-    return Math.floor(Math.random() * 256);
-}
+// // Hovering Effect RANDOM COLOR BACKGROUND
+// function randomRBG() {
+//     return Math.floor(Math.random() * 256);
+// }
 
+// grids.forEach(grid => {
+//     grid.addEventListener("mouseover", () => {
+//         grid.style.backgroundColor = `rgb(${randomRBG()},${randomRBG()},${randomRBG()})`;
+//     });
+//     grid.addEventListener("mouseleave", () => {
+//         grid.style.backgroundColor = "white";
+//     })
+// })
+
+
+// Hover to add opacity 10% on each pass
 grids.forEach(grid => {
     grid.addEventListener("mouseover", () => {
-        grid.style.backgroundColor = `rgb(${randomRBG()},${randomRBG()},${randomRBG()})`;
+        grid.style.backgroundColor = `black`;
+        grid.style.opacity = (parseFloat(grid.style.opacity) || 0) + 0.1;
     });
-    grid.addEventListener("mouseleave", () => {
-        grid.style.backgroundColor = "white";
-    })
-})
+});
